4967bce81a97cef7bc335a892cf278e34f8c8890
user <jonathan.caron04@gmail.com>
2017-10-05 10:47:28 +0200

    bootstrap 1.0.1

--- index.php
+++ index.php
@@ -12,14 +12,10 @@
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/imagehover.min.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
-    <!-- =======================================================
-        Theme Name: Mentor
-        Theme URL: https://bootstrapmade.com/mentor-free-education-bootstrap-theme/
-        Author: BootstrapMade.com
-        Author URL: https://bootstrapmade.com
-    ======================================================= -->
+
   </head>
   <body>
+    <?php include('./requires/dblog.php'); ?>
     <!--Navigation bar-->
     <nav class="navbar navbar-default navbar-fixed-top">
       <div class="container">
@@ -29,7 +25,7 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="index.html">Men<span>tor</span></a>
+        <a class="navbar-brand" href="index.php">Men<span>tor</span></a>
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
         <ul class="nav navbar-nav navbar-right">
@@ -37,7 +33,7 @@
           <li><a href="#organisations">Organisations</a></li>
           <li><a href="#courses">Courses</a></li>
           <li><a href="#pricing">Pricing</a></li>
-          <li><a href="#" data-target="#login" data-toggle="modal">Sign in</a></li>
+          <li><a href="#" data-target="#login" data-toggle="modal">Connexion</a></li>
           <li class="btn-trial"><a href="#footer">Contact</a></li>
         </ul>
         </div>
@@ -52,36 +48,36 @@
         <div class="modal-content">
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal">&times;</button>
-            <h4 class="modal-title text-center form-title">Login</h4>
+            <h4 class="modal-title text-center form-title">Connexion</h4>
           </div>
           <div class="modal-body padtrbl">
 
             <div class="login-box-body">
-              <p class="login-box-msg">Sign in to start your session</p>
+              <p class="login-box-msg">Connexion au panel utilisateur</p>
               <div class="form-group">
-                <form name="" id="loginForm">
-                 <div class="form-group has-feedback"> <!----- username -------------->
-                      <input class="form-control" placeholder="Username"  id="loginid" type="text" autocomplete="off" />
-            <span style="display:none;font-weight:bold; position:absolute;color: red;position: absolute;padding:4px;font-size: 11px;background-color:rgba(128, 128, 128, 0.26);z-index: 17;  right: 27px; top: 5px;" id="span_loginid"></span><!---Alredy exists  ! -->
-                      <span class="glyphicon glyphicon-user form-control-feedback"></span>
-                  </div>
-                  <div class="form-group has-feedback"><!----- password -------------->
-                      <input class="form-control" placeholder="Password" id="loginpsw" type="password" autocomplete="off" />
-            <span style="display:none;font-weight:bold; position:absolute;color: grey;position: absolute;padding:4px;font-size: 11px;background-color:rgba(128, 128, 128, 0.26);z-index: 17;  right: 27px; top: 5px;" id="span_loginpsw"></span><!---Alredy exists  ! -->
-                      <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                  </div>
-                  <div class="row">
-                      <div class="col-xs-12">
-                          <div class="checkbox icheck">
-                              <label>
-                                <input type="checkbox" id="loginrem" > Remember Me
-                              </label>
-                          </div>
-                      </div>
-                      <div class="col-xs-12">
-                          <button type="button" class="btn btn-green btn-block btn-flat" onclick="userlogin()">Sign In</button>
-                      </div>
-                  </div>
+                <form name="connexion" id="loginForm" action="profil.php" method="post">
+                             <div class="form-group has-feedback"> <!----- username -------------->
+                                  <input class="form-control" placeholder="Identifiant"  name="login" id="loginid" type="text" autocomplete="off" />
+                        <span style="display:none;font-weight:bold; position:absolute;color: red;position: absolute;padding:4px;font-size: 11px;background-color:rgba(128, 128, 128, 0.26);z-index: 17;  right: 27px; top: 5px;" id="span_loginid"></span><!---Alredy exists  ! -->
+                                  <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                              </div>
+                              <div class="form-group has-feedback"><!----- password -------------->
+                                  <input class="form-control" placeholder="Mot de passe" name="password" id="loginpsw" type="password" autocomplete="off" />
+                        <span style="display:none;font-weight:bold; position:absolute;color: grey;position: absolute;padding:4px;font-size: 11px;background-color:rgba(128, 128, 128, 0.26);z-index: 17;  right: 27px; top: 5px;" id="span_loginpsw"></span><!---Alredy exists  ! -->
+                                  <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                              </div>
+                              <div class="row">
+                                  <div class="col-xs-12">
+                                      <div class="checkbox icheck">
+                                          <label>
+                                            <input type="checkbox" name="checkbox" id="loginrem" > Retenir mes identifiants
+                                          </label>
+                                      </div>
+                                  </div>
+                                  <div class="col-xs-12">
+                                      <button type="submit" value="Submit" class="btn btn-green btn-block btn-flat" onclick="userlogin()">Valider</button>
+                                  </div>
+                              </div>
                 </form>
               </div>
             </div>
