ccb60a71eb977c52f445ffe45a1a4717d33ae1b6
user <jonathan.caron04@gmail.com>
2017-10-05 11:29:15 +0200

    bootstrap 1.0.3

--- profil.php
+++ profil.php
@@ -1,27 +1,82 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Mentor Education Bootstrap Theme</title>
-    <meta name="description" content="Free Bootstrap Theme by BootstrapMade.com">
-    <meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
-
-    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans|Candal|Alegreya+Sans">
-    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/imagehover.min.css">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-
+    <?php include('./requires/header.php'); ?>
   </head>
   <body>
     <?php include('./requires/dblog.php'); ?>
     <?php  include('./includes/navbar.php'); ?>
 
 
-    <div class="container text-center">
+    <section id ="feature" class="section-padding">
+      <br>
+      <div class="container">
+        <div class="row">
+          <div class="header-section col-md-3">
+            <br>
+            <h2>Votre profil</h2>
+             <img src="./img/mentor.jpg" class="img-rounded" width="220" height="200">
+            <p>Jonathan Caron<br>Développeur web</p>
+            <img src="./img/certifie.png" class="img-rounded"  width="50" height="50">
+            <img src="./img/cvok.png" class="img-rounded"  width="35" height="35">
 
-    </div>
+          </div>
+          <div class="header-section col-md-1">
+
+          </div>
+          <div class="header-section col-md-4">
+            <br>
+
+
+          </div>
+          <div class="header-section col-md-4">
+            <br>
+
+          </div>
+          <div class=" container col-md-12">
+            <hr class="bottom-line">
+          </div>
+
+
+
+          <div class="feature-info">
+            <div class="fea">
+              <div class="col-md-4">
+                <div class="heading pull-right">
+                  <h4>Latest Technologies</h4>
+                  <p>Donec et lectus bibendum dolor dictum auctor in ac erat. Vestibulum egestas sollicitudin metus non urna in eros tincidunt convallis id id nisi in interdum.</p>
+                </div>
+                <div class="fea-img pull-left">
+                  <i class="fa fa-css3"></i>
+                </div>
+              </div>
+            </div>
+            <div class="fea">
+              <div class="col-md-4">
+                <div class="heading pull-right">
+                  <h4>Toons Background</h4>
+                  <p>Donec et lectus bibendum dolor dictum auctor in ac erat. Vestibulum egestas sollicitudin metus non urna in eros tincidunt convallis id id nisi in interdum.</p>
+                </div>
+                <div class="fea-img pull-left">
+                  <i class="fa fa-drupal"></i>
+                </div>
+              </div>
+            </div>
+            <div class="fea">
+              <div class="col-md-4">
+                <div class="heading pull-right">
+                  <h4>Award Winning Design</h4>
+                  <p>Donec et lectus bibendum dolor dictum auctor in ac erat. Vestibulum egestas sollicitudin metus non urna in eros tincidunt convallis id id nisi in interdum.</p>
+                </div>
+                <div class="fea-img pull-left">
+                  <i class="fa fa-trophy"></i>
+                </div>
+              </div>
+            </div>
+        </div>
+        </div>
+      </div>
+    </section>
 
 
     <?php  include('./includes/footer.php'); ?>
