9deb5c4cb8a7df3e26bf58ff6eb02988cbc81637
user <jonathan.caron04@gmail.com>
2017-10-09 11:30:57 +0200

    bootstrap 1.2.2.1 - finalisation reg1.php, début mysql vers reg2.php

--- reg1.php
+++ reg1.php
@@ -56,14 +56,17 @@
                 <?php } ?>
                 <?php if($pStatus==1 || $pStatus==2) { ?>
                 <h5>Nous allons, ensemble, finaliser votre inscription.</h5>
-                <h5>Pour que vous ayez accès à toutes les fonctionnalités du site, et par mesure de sécurité, nous allons effectuer une vérification de votre compte.</h5><br>
+                <h5>Pour que vous ayez accès complet à toutes les fonctionnalités du site, et par mesure de sécurité, nous allons effectuer une vérification de votre compte.</h5><br>
                 <h5>Vous êtes actuellement enregistré en tant qu'entreprise. Si ce n'est pas le cas, merci de prendre contact avec notre support à l'adresse suivante support@localhost.</h5><br>
-                <h5>Merci de compléter les informations ci-dessous :</h5>
-                <form class="" action="index.html" method="post">
+                <h5>Merci de bien vouloir compléter les informations suivantes :</h5>
+                <form class="" action="./reg2.php" method="post">
                   <input class="form-control" placeholder="Dénomination" name="entDenomination" type="text" value=<?php echo"$pEntreprise" ?>>
                   <input class="form-control" placeholder="Numéro d'entreprise" name="entNum" type="text" />
                   <input class="form-control" placeholder="Numéro de TVA" name="entTVA" type="text" />
+                  <input class="form-control" placeholder="Une adresse mail de votre entreprise (ex: admin@api-job.be) ou numéro de téléphone où nous pourrons vous joindre" name="entTVA" type="text" />
+                  <button type="submit" value="Submit" class="btn btn-green btn-block btn-flat" onclick="userlogin()">Valider</button>
                 </form>
+                <br><br><br>
                 <?php }
                 else{ ?>
                   <h5>Nous allons, ensemble, finaliser votre inscription.</h5>
