94e17366625e7f86885d9f6dd26ea0bae775d99d
user <jonathan.caron04@gmail.com>
2017-10-12 14:04:19 +0200

    bootstrap 2.2.4.3 - création message.php et connexion avec bdd

--- message.php
+++ message.php
@@ -35,10 +35,42 @@
                 <div class="fea">
                   <div class="col-sm-12 col-md-12">
                     <p>ID du message <?php echo $mid ?> </p>
-                    <form>
-  <input type="button" value="Retourner à la page précédente" onclick="history.back()">
-  </input>
-</form>
+                      <img src="./img/return.png" type="button" value="Retourner à la page précédente" onclick="history.back()" width="50px" height="50px"><br>
+
+                  <table class="table contBorder">
+
+                      <tr>
+                        <th class="col-md-1">De</th>
+                        <td>
+                          <?php
+                          $query=$bdd->prepare('SELECT prenom, nom FROM users WHERE id = :id');
+                          $query->bindValue(':id',$mde, PDO::PARAM_STR);
+                          $query->execute();
+                          $data=$query->fetch();
+
+                          $str1=$data['prenom'];
+                          $str2=$data['nom'];
+                          $str = "$str1 $str2";
+                          if (strlen($str) > 20)
+                          $str = substr($str, 0, 17);
+                          echo $str;
+                          ?>
+                        </td>
+                      </tr>
+                      <tr>
+                        <th>Objet</th>
+                        <td><?php echo $mtitre ?></td>
+                      </tr>
+                      <tr>
+                        <th>Message</th>
+                        <td><?php echo $mmessage ?></td>
+                      </tr>
+
+                    <tbody>
+
+
+                    </tbody>
+                  </table>
                   </div>
                 </div>
             </div>
